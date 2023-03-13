4c5bd081ffe6a3e2b3c36ca91ba649b53e669e2b
HagerAlwe <125517587+HagerAlwe@users.noreply.github.com>
2023-03-13 03:34:00 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let i = 1;
+
+function play() {
+    let x = document.getElementById("main");
+    x.innerHTML = i;
+    i++;
+}
+
+function survey() {
+  let x = document.getElementById("main");
+  x.innerHTML = 0;
+  i = 1;
+}
+
